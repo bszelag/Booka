83b6bafed9886fede99eda308fcd5aab46071056
MGniew <218138@student.pwr.edu.pl>
2017-04-15 17:35:59 +0200

    Added missing ralation, and sequences

--- src/main/java/com/boot/models/Address.java
+++ src/main/java/com/boot/models/Address.java
@@ -2,13 +2,21 @@ package com.boot.models;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import java.io.Serializable;
 
 @Entity
 public class Address implements Serializable {
 
     @Id
+    @SequenceGenerator(name="address_idaddress_seq",
+            sequenceName="address_idaddress_seq",
+            allocationSize=1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator="address_idaddress_seq")
     private Integer id;
 
     @Column(nullable = false)
