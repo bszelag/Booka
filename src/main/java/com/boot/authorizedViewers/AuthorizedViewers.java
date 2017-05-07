c2581720fa334ac39de63b993965f068da16707b
MilBia <218295@student.pwr.wroc.pl>
2017-05-07 21:05:58 +0200

    bd_correction

---
+++ src/main/java/com/boot/authorizedViewers/AuthorizedViewers.java
@@ -0,0 +1,22 @@
+package com.boot.authorizedViewers;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import java.io.Serializable;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthorizedViewers implements Serializable{
+
+    @EmbeddedId
+    private AuthorizedViewersId authorizedViewer;
+
+}
---
+++ src/main/java/com/boot/authorizedViewers/AuthorizedViewersId.java
@@ -0,0 +1,29 @@
+package com.boot.authorizedViewers;
+
+import com.boot.user.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.Embeddable;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+
+@Embeddable
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthorizedViewersId implements Serializable{
+
+    @ManyToOne(optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private User owner;
+
+    @ManyToOne(optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private User authorizedViewer;
+}
