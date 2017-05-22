44d65057e74174a561bd5298e7965cbf43c26096
MilBia <218295@student.pwr.wroc.pl>
2017-05-23 01:43:42 +0200

    rest api contacts

--- src/main/java/com/boot/contact/impl/ContactServiceImpl.java
+++ src/main/java/com/boot/contact/impl/ContactServiceImpl.java
@@ -2,26 +2,17 @@ package com.boot.contact.impl;
 
 import com.boot.contact.ContactService;
 import com.boot.contact.model.Contact;
-import com.boot.contact.model.ContactId;
 import com.boot.contact.repository.ContactRepository;
-import com.boot.user.model.User;
-import com.boot.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
-import java.util.Optional;
 
 @Component
 public class ContactServiceImpl implements ContactService{
 
     @Autowired
     private ContactRepository contactRepository;
-
-    @Autowired
-    private UserRepository userRepository;
 
     @Override
     public Collection<Contact> getContacts(Integer user_id) {
