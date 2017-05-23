f11f329c04ccda01de10fbef545ee5ce3117e695
MilBia <218295@student.pwr.wroc.pl>
2017-05-23 21:55:50 +0200

    rest api friends

--- src/main/java/com/boot/institution/InstitutionController.java
+++ src/main/java/com/boot/institution/InstitutionController.java
@@ -11,19 +11,19 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Collection;
 
 @RestController
-@RequestMapping(value = "/api/v1/Institutions")
+@RequestMapping(value = "/api/v1/institutions")
 public class InstitutionController {
 
     @Autowired
     private InstitutionService institutionService;
 
     @RequestMapping(value = "", method = RequestMethod.GET)
-    public Collection<Institution> getAllInstitution(){
+    public Collection<Institution> getAllInstitutions(){
         return institutionService.getAllInstitutions();
     }
 
     @RequestMapping(value = "{institution_id}", method = RequestMethod.GET)
-    public Collection<Department> getAllInstitution(@PathVariable String institution_id){
+    public Collection<Department> getInstitutionDepartments(@PathVariable String institution_id){
         return institutionService.getInstitutionDepartments(institution_id);
     }
 }
