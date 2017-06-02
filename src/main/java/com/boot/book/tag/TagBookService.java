7d6690820ef536c7cd7579efdeff499f2a99e890
MilBia <218295@student.pwr.wroc.pl>
2017-06-02 14:10:15 +0200

    add tagBookService

---
+++ src/main/java/com/boot/book/tag/TagBookService.java
@@ -0,0 +1,21 @@
+package com.boot.book.tag;
+
+import com.boot.book.model.Book;
+import com.boot.book.tag.model.Tag;
+import com.boot.book.tag.model.TagBook;
+import com.boot.book.tag.model.TagBookId;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+
+@Service
+public interface TagBookService {
+
+    Collection<Book> getBooksByTag(Tag tag);
+
+    Collection<Tag> getBookTags(Book book);
+
+    TagBook addTagToBook(TagBook tagBook);
+
+    void removeTagFromBook(TagBookId tagBookId);
+}
--- src/main/java/com/boot/book/tag/repository/TagBookRepository.java
+++ src/main/java/com/boot/book/tag/repository/TagBookRepository.java
@@ -1,8 +1,28 @@
 package com.boot.book.tag.repository;
 
+import com.boot.book.model.Book;
+import com.boot.book.tag.model.Tag;
 import com.boot.book.tag.model.TagBook;
 import com.boot.book.tag.model.TagBookId;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Collection;
 
 public interface TagBookRepository extends CrudRepository<TagBook, TagBookId> {
+
+    @Query("Select b" +
+            " FROM TagBook t" +
+            " JOIN t.tagBook.tagTittle tt" +
+            " JOIN t.tagBook.book b " +
+            " WHERE tt = :tag")
+    Collection<Book> getBooksByTag(@Param("tag") Tag tag);
+
+    @Query("Select tt" +
+            " FROM TagBook t" +
+            " JOIN t.tagBook.tagTittle tt" +
+            " JOIN t.tagBook.book b " +
+            " WHERE t = :book")
+    Collection<Tag> getBookTags(@Param("book") Book book);
 }
