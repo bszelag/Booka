06eb0b7f23f8d92ca83c5a5c2bf30ed7436cf3b9
MilBia <218295@student.pwr.wroc.pl>
2017-06-04 04:33:21 +0200

    getUserBook correction

--- src/main/java/com/boot/book/BookController.java
+++ src/main/java/com/boot/book/BookController.java
@@ -13,7 +13,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
 import java.util.Objects;
